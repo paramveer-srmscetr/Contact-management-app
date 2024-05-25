1cd1e74cc985ca5f6bf964866093ead9ba8ba47a
Paramveer singh <82169480+paramveer-srmscetr@users.noreply.github.com>
2024-05-25 12:08:56 +0530

    Update contactslice.ts

--- src/features/contacts/contactslice.ts
+++ src/features/contacts/contactslice.ts
@@ -1,4 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { RootState } from '../../app/store';
 
 export interface Contact {
   id: string;
@@ -7,32 +8,47 @@ export interface Contact {
   phone: string;
 }
 
-interface ContactsState {
+interface ContactState {
   contacts: Contact[];
 }
 
-const initialState: ContactsState = {
-  contacts: [],
+const loadContactsFromLocalStorage = (): Contact[] => {
+  const storedContacts = localStorage.getItem('contacts');
+  return storedContacts ? JSON.parse(storedContacts) : [];
+};
+
+const saveContactsToLocalStorage = (contacts: Contact[]) => {
+  localStorage.setItem('contacts', JSON.stringify(contacts));
+};
+
+const initialState: ContactState = {
+  contacts: loadContactsFromLocalStorage(),
 };
 
 const contactSlice = createSlice({
   name: 'contacts',
   initialState,
   reducers: {
-    addContact(state, action: PayloadAction<Contact>) {
+    addContact: (state, action: PayloadAction<Contact>) => {
       state.contacts.push(action.payload);
+      saveContactsToLocalStorage(state.contacts);
     },
-    editContact(state, action: PayloadAction<Contact>) {
+    editContact: (state, action: PayloadAction<Contact>) => {
       const index = state.contacts.findIndex(contact => contact.id === action.payload.id);
       if (index !== -1) {
         state.contacts[index] = action.payload;
+        saveContactsToLocalStorage(state.contacts);
       }
     },
-    deleteContact(state, action: PayloadAction<string>) {
+    deleteContact: (state, action: PayloadAction<string>) => {
       state.contacts = state.contacts.filter(contact => contact.id !== action.payload);
+      saveContactsToLocalStorage(state.contacts);
     },
   },
 });
 
 export const { addContact, editContact, deleteContact } = contactSlice.actions;
+
+export const selectContacts = (state: RootState) => state.contacts.contacts;
+
 export default contactSlice.reducer;
